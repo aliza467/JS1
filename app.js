e125d207390f69579967e12ca4143f6d2c678ca4
aliza <ak6614462@gmail.com>
2024-02-22 22:50:49 +0500

    JS

---
+++ app.js
@@ -0,0 +1,146 @@
+// CHAPTER 1
+// Q1
+alert("Welcome visitors");
+
+// Q2
+alert("Error! Please enter a valid password.")
+
+// Q3
+alert("Welcome to JS Land\n Happy Coding!")
+
+// Q4
+alert("Welcome to JS Land.")
+
+// Q5
+// console.log("Hello... Ican run JS through my web browser's console.")
+
+
+// CHAPTER 2
+// Q1
+var username;
+
+// Q2
+var myName="Aliza Khan";
+
+// Q3
+var message="Hello World";
+alert(message);
+
+// Q4
+var fname="Jhone Doe"
+var age="15 years old"
+var course="Certified Mobile Application Development"
+alert(fname)
+alert(age)
+alert(course)
+
+// Q5
+var pizza=("PIZZA\nPIZZ\nPIZZ\nPIZ\nPI\nP")
+alert(pizza)
+
+// Q6
+var email="ak6614462@gmail.com"
+alert("My email address is "+ email)
+
+// Q7
+var book="A smarter way to learn JavaScript"
+alert("I am trying to learn from the book "+ book)
+
+// Q8
+// document.write("Yah! I can write HTML content through JS.")
+
+// Q9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// CHAPTER 03
+// Q1
+var age="I am 15 years old"
+alert(age)
+
+// Q2
+var N=14
+alert("You have visited this site "+N +"times")
+
+// Q3
+// var birthyear=2004
+// document.write("My birthyear is "+birthyear+" <br> Data type of my declared variable is "+typeof(birthyear))
+
+
+// Q4
+// var visitorname="John Doe"
+// var product="Tshirt"
+// var quantity=5
+// document.write(visitorname + " ordered "+ quantity +" "+product+" on XYZ clothing store")
+
+
+// CHAPTER 1
+// Q1
+var firstname="aliza"
+var lastname="khan"
+var email="ak6614462@gmail.com"
+var password="alizee"
+var phonenumber="03302020202"
+
+alert(firstname)
+alert(lastname)
+alert(email)
+alert(phonenumber)
+alert(password)
+
+
+// Q2
+alert("You're learning JS!")
+
+// Q3
+alert("Hello World")
+
+
+// CHAPTER2
+
+// Q1
+var MyName="Aliza"
+
+// Q2
+var lastname;
+lastname="khan"
+
+// Q3
+var teamName="Barcelona"
+alert(teamName)
+
+// Q4
+var bestMan="Johnny"
+
+// CHAPTER 3
+
+// Q1
+var caseQty;
+
+// Q2
+caseQty=144;
+
+// Q3
+var num=9;
+
+// Q4
+var add;
+add=2+3;
+alert(add)
+
+// Q5
+// var merchtotal=100;
+// var shippingcharge=10;
+// var ordertotal=merchtotal+shippingcharge;
+// alert(ordertotal)
+// the value is 110.
+
+// Q6
+var num=21;
+num=num+1;
+alert(num)
+
+
+
+
+
+
